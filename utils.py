d872c665ddb3673966a92c523e399a06d902ca17
Terrence Plunkett <eightys3v3n+github@gmail.com>
2017-03-22 17:42:33 -0600

    added __str__ __repr__ to Position

--- utils.py
+++ utils.py
@@ -3,3 +3,25 @@ class Position:
     self.x = x
     self.y = y
     self.z = z
+
+
+  def __add__(self, o):
+    res = Position(0, 0, 0)
+    res.x = self.x + o.x
+    res.y = self.y + o.y
+    res.z = self.z + o.z
+    return res
+
+
+  def __radd__(self, o):
+    self.x += o.x
+    self.y += o.y
+    self.z += o.z
+
+
+  def __str__(self):
+    return "("+str(self.x)+", "+str(self.y)+", "+str(self.z)+")"
+
+
+  def __repr__(self):
+    return self.__str__()
