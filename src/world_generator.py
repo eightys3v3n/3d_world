5b584eaec78b22ed27039df2c5194df36969b1c3
EightyS3v3n <eightys3v3n@gmail.com>
2019-08-01 21:09:25 -0600

    Some comments and docs

--- src/world_generator.py
+++ src/world_generator.py
@@ -1,4 +1,5 @@
 import multiprocessing as mp
+import unittest
 
 
 class DefaultGeneration:
@@ -10,13 +11,18 @@ class DefaultGeneration:
 
 
 class WorldGenerator:
+    """Controls WorldGeneratorSlaves and handles requesting chunks. An instance of this is held by the main game thread."""
     def __init__(self, world_client, parent_log):
-        self.__running__ = mp.Value('b', True)
+        """Parameters:
+            world_client (WorldDataClient): An instance of WorldDataClient that can be used to access the world data.
+            parent_log (logging.Logger): The logger that the calling function uses.
+        """
+        self.__running__ = mp.Value('b', True) # Used to stop any running threads.
         self.parent_log = parent_log
         self.log = self.parent_log.getChild("WorldGenerator")
-        self.world_client = world_client
-        self.chunks_to_generate = mp.Queue(config.WorldGenerator.RequestQueueSize)
-        self.recently_requested = {}
+        self.world_client = world_client # Used to access the world data
+        self.chunks_to_generate = mp.Queue(config.WorldGenerator.RequestQueueSize) # A queue of chunks to generate.
+        self.recently_requested = {} # A list of chunks that have been recently requested and the time requested. These won't be requested again until removed.
 
         self.generators = []
         for i in range(config.WorldGenerator.Processes):
@@ -26,6 +32,9 @@ class WorldGenerator:
 
 
     def garbage_collect_recently_requested(self, *args):
+        """Remove any chunk positions in recently_requested that are older than config.WorldGenerator.RecentlyRequested.
+
+        Parameters: Ignores all arguments"""
         self.log.info("Cleaning recently requested")
         for (cx, cy), t in self.recently_requested.items():
             if time.time() - t >= config.WorldGenerator.RecentlyRequested:
@@ -33,6 +42,13 @@ class WorldGenerator:
 
 
     def request_chunk(self, cx, cy):
+        """If a chunk has not been recently requested (config.WorldGenerator.RecentlyRequested) then request it to be generated.
+        Also calls garbage_collect_recently_requested if the size is above maximum.
+
+        Parameters:
+            cx (int): The chunk x coordinate of the request
+            cy (int): The chunk y coordinate of the request
+        """
         if (cx, cy) in self.recently_requested: return
         if len(self.recently_requested) > config.WorldGenerator.MaxRecentChunksStored:
             self.log.warning("Had to clean up recently_requested on main process because size is {}".format(len(self.recently_requested)))
@@ -48,10 +64,12 @@ class WorldGenerator:
 
     @classmethod
     def pick_generation(cls, cx, cy):
+        """To be used in future to pick biomes or such. Currently just gets the DefaultGeneration class."""
         return DefaultGeneration
 
 
 class WorldGenerationSlave(mp.Process):
+    """A slave that is created by WorldGenerator. This class actually does the generation of chunks in separate processes."""
     def __init__(self, running, world_client, parent_log, chunks_to_generate):
         super(WorldGenerationSlave, self).__init__()
         self.chunks_to_generate = chunks_to_generate
@@ -67,3 +85,8 @@ class WorldGenerationSlave(mp.Process):
             generation_type = WorldGenerator.pick_generation(cx, cy)
             chunk = generation_type.generate(cx, cy)
             self.world_client.init_chunk(cx, cy)
+
+
+class TestWorldGeneration(unittest.TestCase):
+    def test_request_chunk(self):
+        self.fail()
